8b87927b6ba717be02cf8c7c4c02d6d0ce8b35ac
Facundo Rivera <facuriive@gmail.com>
2021-05-15 00:42:10 -0300

    Js solucionado

--- js/gula.js
+++ js/gula.js
@@ -1,66 +1,15 @@
-function OpenSide() {
-    //Index
-    if (document.getElementById("index")) {
-        document.getElementById("banner").style.display = "none";
-        document.getElementById("btnBurger").style.display = "none"
-        document.getElementById("mySidevar").style.display = "block";
-    }
-        //Nosotros
-
-    if (document.getElementById("us")) {
-        document.getElementById("close").style.display = "none";
-        document.getElementById("mySidevar").style.display = "block";
-        document.getElementById("btnBurger").style.display = "none"
-    }
-        //Conctacto
-    if (document.getElementById("contact")){
-        document.getElementById("sideContact").style.display = "none";
-        document.getElementById("mySidevar").style.display = "block";
-        document.getElementById("btnBurger").style.display = "none"
-    }
-    if(document.getElementById("gulas")){
-        document.getElementById("products").style.display="none"
-        document.getElementById("mySidevar").style.display="block"
-        document.getElementById("btnBurger").style.display="none";
-    }
-    if(document.getElementById("galery")){
-        document.getElementById("galeria").style.display="none"
-        document.getElementById("mySidevar").style.display = "block";
-        document.getElementById("btnBurger").style.display = "none"
-    }
-    
+$(document).ready(function () {
+    $("#btnBurger").click(function () {
+      $("#mySidevar").removeClass("showMenu");
+      $("#banner").addClass("showMenu");
+    });
+  });
 
 
+  $(document).ready(function () {
+    $(".btnClose").click(function () {
+      $("#mySidevar").addClass("showMenu");
+      $("#banner").removeClass("showMenu");  
+    });
+  });
 
-
-}
-
-function CloseSide() {
-    if (document.getElementById("index")) {
-        document.getElementById("mySidevar").style.display = "none";
-        document.getElementById("banner").style.display = "block";
-        document.getElementById("btnBurger").style.display = "block"
-    }
-
-    if (document.getElementById("us")) {
-        document.getElementById("mySidevar").style.display = "none";
-        document.getElementById("close").style.display="grid";
-        document.getElementById("btnBurger").style.display = "block"
-    }
-    if (document.getElementById("contact")){
-        document.getElementById("mySidevar").style.display = "none";
-        document.getElementById("sideContact").style.display = "grid";
-        document.getElementById("btnBurger").style.display = "block"
-    }
-    if(document.getElementById("gulas")){
-        document.getElementById("mySidevar").style.display="none";
-        document.getElementById("products").style.display="grid";
-        document.getElementById("btnBurger").style.display="block";
-    }
-    if(document.getElementById("galery")){
-        document.getElementById("mySidevar").style.display="none";
-        document.getElementById("galeria").style.display="grid";
-        document.getElementById("btnBurger").style.display="block";
-    }
-
-}
